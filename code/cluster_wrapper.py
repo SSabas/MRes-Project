fd5e1cac15ef0929f55032bb2cbfe9040a45a62b
Sven Sabas <SSabas@gmail.com>
2017-08-19 19:25:03 +0300

    Code for empirical analysis
    Added Python wrapper function for the Fortran clustering script.

---
+++ code/cluster_wrapper.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Project: Imperial MRes Dissertation Project
+Description: Python wrapper script to run cluster simulation based on
+Fortran code.
+    
+Author: Sven Sabas
+Date: 19/08/2017
+"""
+
+######################## IMPORT REQUIRED SCRIPT ##############################
+
+import numpy as np
+
+################################# END ########################################
