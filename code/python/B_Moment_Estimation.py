4e0563fe0e3cc66804d404348d9ac76ccc869873
Sven Sabas <ssabas@gmail.com>
2018-06-14 10:32:58 +0100

    Changed directory structure (included results folder).

--- code/python/B_Moment_Estimation.py
+++ code/python/B_Moment_Estimation.py
@@ -30,36 +30,6 @@ from scipy.optimize import curve_fit
 # -------------------- CURVE FITTING ----------------------------- #
 
 
-# Define a function for the exponential to be fitted
-def exp_function(x, a, b):
-    return a * np.exp(-b * x)
-
-
-# Fit the curve and get the residuals
-def fit_exp_function(exp_function, data, to_plot='yes'):
-
-    # Extract relevant series
-    ydata = np.array(data)
-    xdata = range(1, len(ydata)+1)
-
-    # Fit the curve (extract the parameters a and b)
-    parameters, parameters_cov = curve_fit(exp_function, xdata, ydata)
-
-    # Plot the results
-    if to_plot == 'yes':
-        plt.plot(data.index, ydata, 'b-', label='Data')
-        plt.plot(data.index, exp_function(xdata, *parameters), 'r-', label='Fit: a=%5.3f, b=%5.3f' % tuple(parameters))
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.legend()
-        plt.show()
-
-    # Get the residuals
-    residuals = ydata - exp_function(xdata, *parameters)
-
-    return residuals, parameters
-
-
 # Fit the curve and get the residuals
 def exponential_growth(data, to_plot='yes'):
 
@@ -99,50 +69,7 @@ def exponential_growth(data, to_plot='yes'):
     return residuals, parameters
 
 
-# Calculate the moments for scenario simulation
 def calculate_moments(data, to_plot='no'):
-
-    # Dictionary placeholders
-    means = {}
-    variances = {}
-
-    # Calculate the exponential growths
-    for column in data:
-        print(column)
-
-        price_series = data[column]
-        residuals, parameters = exponential_growth(price_series, to_plot=to_plot)
-        means[column] = parameters[1]
-
-    # Calculate the (co-)variances
-    for column_j in data:
-        price_series_j = data[column_j]
-        M_j = np.exp(means[column_j])*np.array(price_series.tail(1))
-
-        # Placeholder for variances
-        variances[column_j] = []
-
-        for column_l in data:
-            price_series_l = data[column_l]
-            M_l = np.exp(means[column_l]) * np.array(price_series.tail(1))
-            C_jl = (1/(data.shape[0]-2+1)) * np.sum((price_series_j-M_j)*(price_series_l-M_l))
-            variances[column_j].append(C_jl)
-
-
-    # Put the covariances to K*K matrix
-    variance_matrix = []
-    mean_matrix = []
-
-    for column_j in data:
-        mean_matrix.append(means[column_j])
-        variance_matrix.append(variances[column_j])
-
-    variance_matrix = np.array(variance_matrix).reshape(data.shape[1], data.shape[1])
-
-    return mean_matrix, variance_matrix
-
-
-def calculate_moments2(data, to_plot='no'):
 
     # Dictionary placeholders
     means = {}
@@ -157,7 +84,7 @@ def calculate_moments2(data, to_plot='no'):
 
     # Calculate the exponential growths and covariances
     for column_j in data:
-        print(column_j)
+        # print(column_j)
 
         price_series_j = data[column_j]
         residuals_j, parameters_j = exponential_growth(price_series_j, to_plot=to_plot)
@@ -191,18 +118,25 @@ def calculate_moments2(data, to_plot='no'):
 
     return mean_matrix, variance_matrix
 
+
 # Output in format required for C++ simulation
-def cpp_layout(file_name, data, branching=(4, 4, 4)):
+def put_to_cpp_layout(folder, file_name, data, branching=(4, 4, 4)):
 
     # Set path
     path = os.getcwd() + '/code/cpp/cluster2/' + file_name
+    path_backup = os.getcwd() + '/results/' + folder + '/' + file_name
+
+    # Check if the directory already exists
+    if not os.path.exists(os.getcwd() + '/results/' + folder):
+        os.makedirs(os.getcwd() + '/results/' + folder)
 
     # Get means and variances
     mean_matrix, variance_matrix = calculate_moments(data)
 
     # Save to designated file in cpp folder
-    print('Saving file named - %s -  in /code/cpp/cluster2 folder.' %file_name)
+    print('Saving results to %s.' % path_backup)
 
+    # Save it in cluster folder to run the simulation programme
     with open(path, "w") as text_file:
         print("ASSETS %d" %(data.shape[1]), file=text_file)
         print("STAGES %d" %(len(branching)), file=text_file)
@@ -218,6 +152,97 @@ def cpp_layout(file_name, data, branching=(4, 4, 4)):
         print("BRANCHING", file=text_file)
         print(" ".join(map(str, branching)), file=text_file)
         print("END", file=text_file)
+
+    # Save a secondary copy to designated folder
+    with open(path_backup, "w") as text_file:
+        print("ASSETS %d" %(data.shape[1]), file=text_file)
+        print("STAGES %d" %(len(branching)), file=text_file)
+        print("COVARIANCE", file=text_file)
+        n = 1
+        for i in variance_matrix:
+            print(" ".join(map(str, i[0:n])), file=text_file)
+            n += 1
+        print("INITIAL", file=text_file)
+        print(" ".join(map(str, np.array(data.tail(1))[0])), file=text_file)
+        print("GROWTH", file=text_file)
+        print(" ".join(map(str, mean_matrix)), file=text_file)
+        print("BRANCHING", file=text_file)
+        print(" ".join(map(str, branching)), file=text_file)
+        print("END", file=text_file)
+
+
+#
+# # Define a function for the exponential to be fitted
+# def exp_function(x, a, b):
+#     return a * np.exp(-b * x)
+#
+#
+# # Fit the curve and get the residuals
+# def fit_exp_function(exp_function, data, to_plot='yes'):
+#
+#     # Extract relevant series
+#     ydata = np.array(data)
+#     xdata = range(1, len(ydata)+1)
+#
+#     # Fit the curve (extract the parameters a and b)
+#     parameters, parameters_cov = curve_fit(exp_function, xdata, ydata)
+#
+#     # Plot the results
+#     if to_plot == 'yes':
+#         plt.plot(data.index, ydata, 'b-', label='Data')
+#         plt.plot(data.index, exp_function(xdata, *parameters), 'r-', label='Fit: a=%5.3f, b=%5.3f' % tuple(parameters))
+#         plt.xlabel('x')
+#         plt.ylabel('y')
+#         plt.legend()
+#         plt.show()
+#
+#     # Get the residuals
+#     residuals = ydata - exp_function(xdata, *parameters)
+#
+#     return residuals, parameters
+
+# # Calculate the moments for scenario simulation
+# def calculate_moments(data, to_plot='no'):
+#
+#     # Dictionary placeholders
+#     means = {}
+#     variances = {}
+#
+#     # Calculate the exponential growths
+#     for column in data:
+#         print(column)
+#
+#         price_series = data[column]
+#         residuals, parameters = exponential_growth(price_series, to_plot=to_plot)
+#         means[column] = parameters[1]
+#
+#     # Calculate the (co-)variances
+#     for column_j in data:
+#         price_series_j = data[column_j]
+#         M_j = np.exp(means[column_j])*np.array(price_series.tail(1))
+#
+#         # Placeholder for variances
+#         variances[column_j] = []
+#
+#         for column_l in data:
+#             price_series_l = data[column_l]
+#             M_l = np.exp(means[column_l]) * np.array(price_series.tail(1))
+#             C_jl = (1/(data.shape[0]-2+1)) * np.sum((price_series_j-M_j)*(price_series_l-M_l))
+#             variances[column_j].append(C_jl)
+#
+#
+#     # Put the covariances to K*K matrix
+#     variance_matrix = []
+#     mean_matrix = []
+#
+#     for column_j in data:
+#         mean_matrix.append(means[column_j])
+#         variance_matrix.append(variances[column_j])
+#
+#     variance_matrix = np.array(variance_matrix).reshape(data.shape[1], data.shape[1])
+#
+#     return mean_matrix, variance_matrix
+
 
 ####################### END ########################
 
