bb9ba6c3f28e01a5af269ba64984159c7e9eb5fb
Sven Sabas <ssabas@gmail.com>
2018-06-19 15:09:12 +0100

    Added portfolio optimisation to the run script pipeline.

--- code/python/F_Run.py
+++ code/python/F_Run.py
@@ -25,14 +25,7 @@ Date:
 import pandas as pd
 import numpy as np
 import os
-import matplotlib.pyplot as plt
-import subprocess
-from scipy.optimize import curve_fit
-from datetime import timedelta
 import sys
-from operator import mul
-from functools import reduce
-
 
 # Add the python scripts folder to system path
 sys.path.insert(0, os.getcwd() + '/code/python')
@@ -41,31 +34,37 @@ from A_Data_Import import *
 from B_Moment_Estimation import *
 from C_Simulation import *
 from D_CPLEX_Solver import *
-
+from E_Evaluation import *
 
 # -------------------- VARIABLES ----------------------------------------------------- #
 
-folder = 'test_run'
 input_file = 'moment_estimations'
 output_file = 'scenario_tree'
-simulations = 40000
-nr_scenarios = 10
-branching = (10, 10, 2)
+simulations = 100000
+nr_scenarios = 4
+branching = (2, 2, 8, 8)
 instruments_NYSE = ['KO', 'MSFT', 'IBM', 'AXP', 'PG', 'DIS', 'INTC', 'FDX', 'ADM', 'MAT']
-instruments_FTSE = ['HSBC', 'VOD', 'BP', 'GSK', 'AZN', 'RIO', 'BG', 'TSCO', 'AAL', 'PRU']
+instruments_FTSE = ['HSBC', 'VOD', 'BP', 'GSK', 'AZN', 'RIO', 'BG', 'TSCO', 'BT', 'PRU']
 instruments = instruments_FTSE #+ instruments_FTSE
-start_date = '2015-01-01'
-end_date = '2017-01-01'
+start_date = '2005-01-01'
+end_date = '2014-01-01'
 source = 'morningstar'
 price_point = 'Close'
 to_plot = 'yes'
+to_save = 'yes'
 initial_portfolio = np.repeat(1/len(instruments), len(instruments)) # Equally weighted portfolio
 beta = 0.99
 cost_to_sell = 0.01
 cost_to_buy = 0.01
-initial_wealth= 1
-return_target = 1.02
+initial_wealth = 1
+return_target = 1.01
 solver = 'gurobi'
+frequency = 'weekly'
+look_back_period = 50
+input_file = 'moment_estimation'
+benchmark = 'yes'
+periods_to_forecast = 5
+folder = 'portfolio_optimisation_%s_weeks' %(periods_to_forecast)
 
 # Bounds for optimisation
 sell_bounds = [[0.0], [0.2]]
@@ -76,14 +75,15 @@ weight_bounds = [[0.0], [1.0]]
 pd.set_option('display.max_columns', 10)
 
 # Specify testing parameters
-returns = np.linspace(1.02, 1.08, 20)
+returns = np.linspace(1.02, 1.10, 20)
 
 # -------------------- EXECUTING C++ CODE THROUGH PYTHON ----------------------------- #
 
 # Get the data
-stock_data = import_stock_data_api(instruments=instruments, data_source= source,
+stock_data = import_stock_data_api(instruments=instruments, data_source=source,
                                    start_date=start_date, end_date=end_date, price_point=price_point,
-                                   to_plot=to_plot, to_save='yes', from_file='no')  # Takes c. 20 secs to query
+                                   to_plot=to_plot, to_save=to_save, from_file='no', folder=folder,
+                                   frequency=frequency)  # Takes c. 20 secs to query
 
 # Test exponential fit for a single stock
 # residuals, parameters = exponential_growth(stock_data['VOD'], to_plot=to_plot)
@@ -92,92 +92,45 @@ stock_data = import_stock_data_api(instruments=instruments, data_source= source,
 # means, variances = calculate_moments(stock_data)
 
 # Get the moments and save to format for the simulator
-put_to_cpp_layout(folder, input_file, stock_data, branching=branching)
+# put_to_cpp_layout(folder, input_file, stock_data, branching=branching)
 
 # Run the simulation
 # clean_cluster() # In case first run
 # compile_cluster() # Gives some errors, but still works
-run_cluster(input_file, output_file, folder, samples=simulations, scenario_trees=nr_scenarios)
+# run_cluster(input_file, output_file, folder, samples=simulations, scenario_trees=nr_scenarios)
 
 # Read the output
-scenarios_dict = read_cluster_output(output_file, folder, scenario_trees=nr_scenarios, asset_names=instruments)
+# scenarios_dict = read_cluster_output(output_file, folder, scenario_trees=nr_scenarios, asset_names=instruments)
 
 # Get the final cumulative probabilities
-scenarios_dict = add_cumulative_probabilities(scenarios_dict, branching)
+# scenarios_dict = add_cumulative_probabilities(scenarios_dict, branching)
 
 # Plot the simulation output for sense-checking
-output = {}
-output_cum = {}
-for i in instruments:
-    print(i)
-    output[i], output_cum[i] = plot_cluster_output(stock_data, i, scenarios_dict['1'], branching, to_plot='yes')
+# output = {}
+# output_cum = {}
+# for i in instruments:
+#     print(i)
+#     output[i], output_cum[i] = plot_cluster_output(stock_data, i, scenarios_dict['1'], branching, to_plot='yes')
+
+# Create an efficient frontier
+# ef_wcvars = efficient_frontier(scenarios_dict, returns, instruments, branching, initial_portfolio,
+#                        sell_bounds, buy_bounds, weight_bounds, cost_to_buy=cost_to_buy, cost_to_sell=cost_to_sell,
+#                        beta=beta, initial_wealth=initial_wealth, to_plot=to_plot, folder=folder, solver=solver,
+#                        to_save=to_save)
+
+# Calculate the optimised portfolio
+optimised_returns, benchmark_returns = portfolio_optimisation(stock_data, look_back_period, start_date, end_date,
+                                                              folder=folder, periods_to_forecast=periods_to_forecast,
+                                                              input_file=input_file, frequency=frequency,
+                                                              benchmark=benchmark, to_plot=to_plot, to_save=to_save,
+                                                              branching=branching, simulations=simulations,
+                                                              initial_portfolio=initial_portfolio,
+                                                              nr_scenarios=nr_scenarios, return_target=return_target,
+                                                              sell_bounds=sell_bounds, buy_bounds=buy_bounds,
+                                                              weight_bounds=weight_bounds, cost_to_buy=cost_to_buy,
+                                                              cost_to_sell=cost_to_sell, beta=beta,
+                                                              initial_wealth=initial_wealth, solver=solver)
+
+# Save the workspace variables to file
 
 
-
-answer, w_0_weights, variables = robust_portfolio_optimisation(scenarios_dict, instruments, branching, initial_portfolio,
-                                  sell_bounds, buy_bounds, weight_bounds, cost_to_buy=0.01, cost_to_sell=0.01,
-                                  beta=0.99, initial_wealth=1, return_target=1.06, to_save='yes', folder=folder)
-
-answer, w_0_weights, variables = robust_portfolio_optimisation(scenarios_dict, instruments, branching, initial_portfolio,
-                                  sell_bounds, buy_bounds, weight_bounds, cost_to_buy=0.01, cost_to_sell=0.01,
-                                  beta=0.95, initial_wealth=1, return_target=1.06, to_save='no', folder='',
-                                  solver='gurobi', wcvar_minimizer='yes')
-
-    wcvars.append(answer.solution.get_objective_value())
-
-return_targets = np.linspace(1, 1.08, 20)
-wcvars = []
-wcvars_1 = []
-wcvars_2 = []
-wcvars_3 = []
-wcvars_4 = []
-
-
-dict_1 = { '1': scenarios_dict['1']}
-dict_2 = { '1': scenarios_dict['2']}
-dict_3 = { '1': scenarios_dict['3']}
-dict_4 = { '1': scenarios_dict['4']}
-
-
-for i in return_targets:
-    print(i)
-    answer, w_0_weights, variables = robust_portfolio_optimisation(scenarios_dict, instruments, branching, initial_portfolio,
-                                      sell_bounds, buy_bounds, weight_bounds, cost_to_buy=0.01, cost_to_sell=0.01,
-                                      beta=0.99, initial_wealth=1, return_target=i, to_save='no', folder=folder)
-    wcvars.append(answer.solution.get_objective_value())
-
-    answer, w_0_weights, variables = robust_portfolio_optimisation(dict_1, instruments, branching, initial_portfolio,
-                                      sell_bounds, buy_bounds, weight_bounds, cost_to_buy=0.01, cost_to_sell=0.01,
-                                      beta=0.99, initial_wealth=1, return_target=i, to_save='no', folder=folder)
-    wcvars_1.append(answer.solution.get_objective_value())
-
-    answer, w_0_weights, variables = robust_portfolio_optimisation(dict_2, instruments, branching, initial_portfolio,
-                                      sell_bounds, buy_bounds, weight_bounds, cost_to_buy=0.01, cost_to_sell=0.01,
-                                      beta=0.99, initial_wealth=1, return_target=i, to_save='no', folder=folder)
-    wcvars_2.append(answer.solution.get_objective_value())
-
-    answer, w_0_weights, variables = robust_portfolio_optimisation(dict_3, instruments, branching, initial_portfolio,
-                                      sell_bounds, buy_bounds, weight_bounds, cost_to_buy=0.01, cost_to_sell=0.01,
-                                      beta=0.99, initial_wealth=1, return_target=i, to_save='no', folder=folder)
-    wcvars_3.append(answer.solution.get_objective_value())
-
-    answer, w_0_weights, variables = robust_portfolio_optimisation(dict_4, instruments, branching, initial_portfolio,
-                                      sell_bounds, buy_bounds, weight_bounds, cost_to_buy=0.01, cost_to_sell=0.01,
-                                      beta=0.99, initial_wealth=1, return_target=i, to_save='no', folder=folder)
-    wcvars_4.append(answer.solution.get_objective_value())
-
-plt.plot(wcvars, return_targets, label='All')
-plt.plot(wcvars_1, return_targets, label='1')
-plt.plot(wcvars_2, return_targets, label='2')
-plt.plot(wcvars_3, return_targets, label='3')
-plt.plot(wcvars_4, return_targets, label='4')
-plt.legend()
-
-plt.savefig('name.png')
-plt.show()
-
-
-
-import matplotlib
-import matplotlib.pyplot
-print(matplotlib.backends.backend)
